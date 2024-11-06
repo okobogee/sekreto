7d120bc1f63790c4a7ddbca10c1262fbb7bae088
okobogee <186433417+okobogee@users.noreply.github.com>
2024-11-06 00:18:09 +0000

    feat: hide download app link when being used offline

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,24 @@
 import styles from '@/styles/header.module.css';
 import { GitHub as GitHubIcon, Download as DownloadIcon } from 'react-feather';
 
+const isOnline = window.location.protocol !== 'file:';
+
 export function Header() {
   return (
     <header className={styles.root}>
       <h1>Sekreto</h1>
       <div className={styles.appVersion}>{__APP_VERSION__}</div>
       <div className={styles.menu}>
-        <a
-          href="./index.html"
-          className={styles.link}
-          download={`sekreto-${__APP_VERSION__}.html`}
-          title="Download app for offline use"
-        >
-          <DownloadIcon size={18} />
-        </a>
+        {isOnline ? (
+          <a
+            href="./index.html"
+            className={styles.link}
+            download={`sekreto-${__APP_VERSION__}.html`}
+            title="Download app for offline use"
+          >
+            <DownloadIcon size={18} />
+          </a>
+        ) : null}
         <a
           href="https://github.com/okobogee/sekreto"
           target="_blank"
