38bf69a3cab7612c1efe99f0f55a1dc3e6612d88
okobogee <186433417+okobogee@users.noreply.github.com>
2024-11-06 00:19:44 +0000

    chore: maintain index.html filename for easier deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,12 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import pkg from './package.json';
-import path from 'path';
-import fs from 'fs';
-
-const renameOutput = {
-  name: 'rename-output',
-  writeBundle() {
-    const newName = `sekreto-${pkg.version}.html`;
-    console.log('Renaming dist/index.html to', newName);
-    fs.renameSync(path.join('dist', 'index.html'), path.join('dist', newName));
-  },
-};
 
 export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
   },
-  plugins: [react(), tsconfigPaths(), viteSingleFile(), renameOutput],
+  plugins: [react(), tsconfigPaths(), viteSingleFile()],
   test: {
     globalSetup: './vitest.global-setup.ts',
     environment: 'jsdom',
