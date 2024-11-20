9dc308e34358ad0543eac3330a37ed0dc061beae
okobogee <186433417+okobogee@users.noreply.github.com>
2024-11-20 00:40:11 +0000

    feat: improve prevent text search message

--- src/hooks/use-prevent-text-search.ts
+++ src/hooks/use-prevent-text-search.ts
@@ -5,7 +5,9 @@ export function usePreventTextSearch() {
     function handleKeyDown(e: KeyboardEvent) {
       if (e.key === 'F3' || (e.key === 'f' && e.ctrlKey)) {
         e.preventDefault();
-        window.alert('Ctrl+F and Cmd+F are disabled for safety reasons. Please scroll the list manually.');
+        window.alert(
+          'Warning: you should never enter your seed phrase words in any electronic device. Make sure to look up your words by scrolling the list manually.'
+        );
       }
     }
     window.addEventListener('keydown', handleKeyDown);
